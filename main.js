b74fd102619dc3d2b9609393f8a8e5ee90ea36c4
R3C0Nx00 <pentestwizard@gmail.com>
2017-12-26 06:31:59 -0500

    Add some designs

--- main.js
+++ main.js
@@ -7,12 +7,11 @@ let mainWindow
 function createWindow () {
   mainWindow = new BrowserWindow({width: 800, height: 600})
   mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
+    pathname: path.join(__dirname, 'app/index.html'),
     protocol: 'file:',
     slashes: true
   }))
   mainWindow.on('closed', function () {
-
     mainWindow = null
   })
 }
