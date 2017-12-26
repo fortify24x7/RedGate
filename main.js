7d94e59fc4ebfa82ba55236eae7806316af4779e
R3C0Nx00 <pentestwizard@gmail.com>
2017-12-26 06:47:10 -0500

    Add image folder

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const path = require('path')
 const url = require('url')
 let mainWindow
 function createWindow () {
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 800, height: 600, resizable: false})
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'app/index.html'),
     protocol: 'file:',
